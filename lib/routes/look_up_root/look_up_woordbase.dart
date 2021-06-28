7b52e2a8f0f61b840fff6941b09d445f48abc5ec
Armagan <unknowncosmicdot@Armagans-MacBook-Air.local>
2021-06-28 19:06:10 +0300

    Look Up page UI is made out

--- lib/routes/look_up_root/look_up_woordbase.dart
+++ lib/routes/look_up_root/look_up_woordbase.dart
@@ -1,19 +1,71 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
-class LookUpPage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String data = '';
+  List newData = [];
+  void fetchFileData() async {
+    String fetchData;
+
+    fetchData = await rootBundle.loadString('TEXT_FILES/wordbase.txt');
+    setState(() {
+      data = fetchData;
+      newData = data.split('\n');
+    });
+  }
+
+  @override
+  void initState() {
+    fetchFileData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.purple[50],
-        child: Center(
-          child: Text(
-            "Look Up",
-            style: TextStyle(
-              fontSize: 40,
+      backgroundColor: Colors.grey[200],
+      body: ListView.builder(
+        itemCount: newData.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: () {},
+            title: Card(
+              child: Row(children: <Widget>[
+                Expanded(
+                  child: Container(
+                    color: Colors.yellow[50],
+                    height: 100,
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(
+                          newData[index],
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.delete,
+                  ),
+                  color: Colors.blue,
+                  highlightColor: Colors.red,
+                  onPressed: () {},
+                )
+                // IconButton(onPressed: () {}, icon: Icons.access_alarms),
+              ]),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
