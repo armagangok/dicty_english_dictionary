3b0dbb7d1f917afadc6e3834b9eee54861a85c3f
Armagan <unknowncosmicdot@Armagans-MacBook-Air.local>
2021-06-28 19:26:29 +0300

    improvments

--- lib/routes/time_looping_route/loop.dart
+++ lib/routes/time_looping_route/loop.dart
@@ -47,64 +47,67 @@ class __IntegerExampleState extends State<_IntegerExample> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              color: Colors.amber[900],
+              child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 40),
+                  child: Text('Choose loop time as minute!',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ))),
+              elevation: 8),
+          SizedBox(height: 20),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.amber[50],
+              border: Border.all(),
             ),
-            color: Colors.amber[900],
-            child: Container(
-                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 40),
-                child: Text('Choose loop time as minute!',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ))),
-            elevation: 8),
-        SizedBox(height: 20),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.amber[50],
-            border: Border.all(),
-          ),
-          child: NumberPicker(
-            textStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            value: _currentIntValue,
-            minValue: 0,
-            maxValue: 40,
-            step: 1,
-            haptics: true,
-            itemHeight: 60,
-            itemWidth: 360,
-            selectedTextStyle: TextStyle(
-                color: Colors.amber[900],
-                fontWeight: FontWeight.bold,
-                fontSize: 50),
-            itemCount: 5,
-            onChanged: (value) => setState(
-              () => _currentIntValue = value,
-            ),
-          ),
-        ),
-        SizedBox(height: 10),
-        TextButton(
-          onPressed: () {
-            print(_currentIntValue);
-          },
-          child: Container(
-            child: Text(
-              "Save",
-              style: TextStyle(
+            child: NumberPicker(
+              textStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              value: _currentIntValue,
+              minValue: 0,
+              maxValue: 40,
+              step: 1,
+              haptics: true,
+              itemHeight: 60,
+              itemWidth: 360,
+              selectedTextStyle: TextStyle(
+                  color: Colors.amber[900],
                   fontWeight: FontWeight.bold,
-                  fontSize: 40,
-                  color: Colors.amber[900]),
+                  fontSize: 50),
+              itemCount: 5,
+              onChanged: (value) => setState(
+                () => _currentIntValue = value,
+              ),
             ),
           ),
-        ),
-      ],
+          SizedBox(height: 10),
+          TextButton(
+            onPressed: () {
+              print(_currentIntValue);
+            },
+            child: Container(
+              child: Text(
+                "Save",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                    color: Colors.amber[900]),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/routes/word_adding_root/add_word.dart
+++ lib/routes/word_adding_root/add_word.dart
@@ -4,47 +4,48 @@ class AddingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      padding: EdgeInsets.symmetric(
-        vertical: 25,
-        horizontal: 10,
-      ),
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-            keyboardType: TextInputType.multiline,
-            maxLines: 10,
-            cursorHeight: 20,
-            cursorColor: Colors.black,
-            maxLength: 2000,
-            style: TextStyle(
-              fontSize: 20,
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 25,
+          horizontal: 10,
+        ),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              keyboardType: TextInputType.multiline,
+              maxLines: 10,
+              cursorHeight: 20,
+              cursorColor: Colors.black,
+              maxLength: 2000,
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Type ',
+                labelText: 'add text',
+                border: OutlineInputBorder(),
+                fillColor: Colors.yellow[100],
+                filled: true,
+              ),
             ),
-            decoration: InputDecoration(
-              hintText: 'Type ',
-              labelText: 'add text',
-              border: OutlineInputBorder(),
-              fillColor: Colors.yellow[100],
-              filled: true,
-            ),
-          ),
-          Container(
-            child: Align(
-              alignment: Alignment.topRight,
-              child: TextButton(
-                onPressed: () {},
-                child: Text(
-                  "Save",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w800,
+            Container(
+              child: Align(
+                alignment: Alignment.topRight,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Save",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.amber[900]),
                   ),
                 ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
