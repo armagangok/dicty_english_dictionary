4c0e2da16b2bd8acc4a5b0d1dc7f85fd92475d57
Armağan Gök <1armagangok@gmail.com>
2022-07-08 10:55:36 +0300

    .

--- lib/view/home/components/future_builder_widget.dart
+++ lib/view/home/components/future_builder_widget.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
+import './dict_data_stack.dart';
 import '../../../../../core/local/database/models/word_hive_model.dart';
 import '../../../../../core/local/database/viewmodels/word_viewmodel.dart';
-
 import '../../view_search_result.dart';
-import 'dict_data_stack.dart';
 
 class FutureBuilderWidget extends StatelessWidget {
   const FutureBuilderWidget({Key? key}) : super(key: key);
@@ -16,33 +15,43 @@ class FutureBuilderWidget extends StatelessWidget {
     return FutureBuilder<dynamic>(
       future: wordInfo,
       builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          Word data1 = Word(
-            word: snapshot.data?.word,
-            origin: snapshot.data?.origin,
-            meaning1: snapshot.data?.meaning1,
-            meaning2: snapshot.data?.meaning2,
-            example: snapshot.data?.example,
-          );
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            if (snapshot.hasData) {
+              Word data1 = Word(
+                word: snapshot.data?.word,
+                origin: snapshot.data?.origin,
+                meaning1: snapshot.data?.meaning1,
+                meaning2: snapshot.data?.meaning2,
+                example: snapshot.data?.example,
+              );
 
-          _wordViewModel.addData(data1).then((value) => null);
-          return Column(
-            children: [
-              DictionaryDataStack(snapshot: snapshot),
-            ],
-          );
-        } else if (snapshot.hasError) {
-          return const Text('Try again or there is no the word in dictionary.');
-        }
+              _wordViewModel.addData(data1).then((value) => null);
+              return Column(
+                children: [
+                  DictionaryDataStack(snapshot: snapshot),
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return const Text(
+                  'Try again or there is no the word in dictionary.');
+            } else {
+              return const Text("An error occured");
+            }
 
-        return SingleChildScrollView(
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height,
-            child: const Center(
+          case ConnectionState.waiting:
+            return const Center(
               child: CircularProgressIndicator(),
-            ),
-          ),
-        );
+            );
+
+          case ConnectionState.none:
+            return const Center(
+              child: Text("Check your internet connection."),
+            );
+
+          default:
+            return const SizedBox();
+        }
       },
     );
   }
