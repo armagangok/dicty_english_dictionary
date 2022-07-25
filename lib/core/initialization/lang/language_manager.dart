2fa1e559687ba5fe596df336861d5b8158ad80bb
Armağan Gök <1armagangok@gmail.com>
2022-07-25 16:03:47 +0300

    .

--- lib/core/initialization/lang/language_manager.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:flutter/material.dart';
-
-// class LanguageManager {
-//   LanguageManager._();
-//   static final instance = LanguageManager._();
-//   final enLocale = const Locale("en");
-
-//   List<Locale> get supportedLocales => [enLocale];
-// }
