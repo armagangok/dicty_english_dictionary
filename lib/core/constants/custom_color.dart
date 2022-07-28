6c0532cceb20db110b4b8445b144534793a48951
Armağan Gök <1armagangok@gmail.com>
2022-07-28 20:21:57 +0300

    update: app version

--- lib/core/constants/custom_color.dart
+++ lib/core/constants/custom_color.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/painting.dart';
+// import 'package:flutter/painting.dart';
 
-class CustomColor {
-  CustomColor._();
-  static final instance = CustomColor._();
+// class CustomColor {
+//   CustomColor._();
 
-  static const britRed = Color(0xffCC0001);
-  static const britBlue = Color(0xff000066);
-  static const white = Color(0xffffffff);
-}
+//   static const britRed = Color(0xffCC0001);
+//   static const britBlue = Color(0xff000066);
+//   static const white = Color(0xffffffff);
+// }
