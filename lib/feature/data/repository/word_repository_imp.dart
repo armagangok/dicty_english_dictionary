a6da9891f963cf848e302dd8691921ab8c85e24c
Armağan Gök <1armagangok@gmail.com>
2022-12-19 22:30:19 +0300

    .

--- lib/feature/data/repository/word_repository_imp.dart
+++ lib/feature/data/repository/word_repository_imp.dart
@@ -1,24 +1,36 @@
-// import 'package:dartz/dartz.dart';
+import 'dart:convert';
 
-// import '../../../core/error/failure.dart';
-// import '../../../core/network/layers/network_executer.dart';
-// import '../../../core/network/request_options.dart/request_options.dart';
-// import '../contract/word_repository.dart';
-// import '../model/word_model.dart';
+import 'package:dartz/dartz.dart';
+import 'package:http/http.dart' as http;
 
-// class WordRepositoryImp extends WordRepository {
-//   @override
-//   Future<Either<Failure, dynamic>> fetchWord({required String word}) async {
-//     try {
-//       dynamic data = await NetworkExecuter.request(
-//         options: MyRequestOptions.instance.requestOptions(
-//           text: word,
-//         ),
-//         responseType: WordModel(),
-//       );
-//       return Right(data);
-//     } catch (e) {
-//       return Left(NetworkFetchFailure());
-//     }
-//   }
-// }
+import '../../../core/error/failure.dart';
+import '../../../global/export/export.dart';
+import '../contract/word_repository.dart';
+
+class WordRepositoryImp implements WordRepository {
+  WordRepositoryImp._();
+  static final instance = WordRepositoryImp._();
+
+  @override
+  Future<Either<Failure, WordModel>> fetchWord({required String word}) async {
+    try {
+      final response = await http.get(
+        Uri.parse('https://api.dictionaryapi.dev/api/v2/entries/en/$word'),
+      );
+
+      if (response.statusCode == 200) {
+        var model = (WordModel.fromMap(jsonDecode(response.body)[0]));
+
+        return Right(model);
+      } else if (response.statusCode == 404) {
+        var decodedErrorMessage = (jsonDecode(response.body));
+
+        return Left(NetWork404Failure(errorMessage: decodedErrorMessage));
+      } else {
+        return Left(NetWorkUnknownFailure());
+      }
+    } on PlatformException {
+      return Left(NetworkFetchFailure());
+    }
+  }
+}
