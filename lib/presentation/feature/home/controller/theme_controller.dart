6204fe9c3c425ce5b8f7feb0f7effbe77d8d006f
Armağan Gök <1armagangok@gmail.com>
2022-12-25 15:35:31 +0300

    .

--- lib/presentation/feature/home/controller/theme_controller.dart
+++
@@ -1,9 +0,0 @@
-// class ThemeController {
-//   ThemeController._();
-//   static final instance = ThemeController._();
-//   // bool switchValue = ThemeService.instance.isSavedDarkMode();
-
-//   bool switchValue = true;
-
-//   void change(bool value) => switchValue = value;
-// }
